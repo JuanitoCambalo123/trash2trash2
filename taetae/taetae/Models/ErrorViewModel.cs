e36901138a70b6260db583188642801f41d0ec7b
JuanitoCambalo123 <145818653+JuanitoCambalo123@users.noreply.github.com>
2023-11-22 00:56:17 +0800

    added tae folder to repository

---
+++ taetae/taetae/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace taetae.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
